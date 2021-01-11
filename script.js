9b1e020b5a6da6308d9faa9028a1e085b84b6825
Susanne Nørgaard <susannenorgaard@Susannes-MacBook-Air.local>
2021-01-12 00:00:20 +0100

    top knap og portfolio

--- script.js
+++ script.js
@@ -19,3 +19,27 @@ function toggleMenu() {
         document.querySelector("#burgerknap").textContent = "✖";
     }
 }
+
+/*<!------------------- til top knap start --------------------->*/
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
